33260b8cd9954f79f7107e7702f765fb7e9bd6d5
ChakruJadhav123 <123489403+ChakruJadhav123@users.noreply.github.com>
2024-04-28 11:48:03 +0000

    conversion of datatypes

---
+++ Basic/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert("Hello") // we are using node js not browser so we cannot use this 
+
+console.log(3+3)
+
+// use mdn and tc39 for documentation 
+
+let name= "Hitesh" //Stringg
+let age = 3 //Integer
+let isLoggeding = true // Boolean
+
+
+//number => 2 to power 53 range
+//bigint => 
+//String // we can use it within single or double quotes
+// Boolean = true/false
+//null =>it is a stand alone value it is a representaion of empty value
+//undefined  => value not assigined
+//symbol => is for unique
+
+//object 
+
+console.log(typeof null); //shows type of variable or value
+console.log(typeof undefined);
---
+++ Basic/03_conversionorOperations.js
@@ -0,0 +1,49 @@
+let score ="33"
+let score2 ="33abc"
+let score3 = null
+let score4 = undefined
+let score5 = true
+
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+console.log(typeof score2);
+let valueInNumberr2 = Number(score2)
+console.log(typeof valueInNumberr2);
+console.log(valueInNumberr2);
+
+console.log(typeof score3);
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+
+console.log(typeof score4);
+let valueInNumber4 = Number(score4)
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4);
+
+console.log(typeof score5)
+let valueInNumberr5 = Number(score5)
+console.log(typeof valueInNumberr5);
+console.log(valueInNumberr5);
+
+let isLoggeding = 1
+let booleanIsLoggeding = Boolean (isLoggeding)
+console.log(booleanIsLoggeding);
+
+
+let someNumeber =33
+let intoString = String(someNumeber)
+console.log(intoString);
+console.log(typeof intoString);
+//"33"==>33
+//"33abc" ==>NaN
+//true ==>1 ;false => 0
+
+// 1==>true
+//0 ==>false
+//""==>false
+//"Hitesh"==true
