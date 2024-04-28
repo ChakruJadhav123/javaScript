5e4faacef7941f359aab3ae783754bbe5c8dc7bb
ChakruJadhav123 <123489403+ChakruJadhav123@users.noreply.github.com>
2024-04-28 12:07:11 +0000

    Operations

--- Basic/03_conversionorOperations.js
+++ Basic/03_conversionorOperations.js
@@ -47,3 +47,45 @@ console.log(typeof intoString);
 //0 ==>false
 //""==>false
 //"Hitesh"==true
+
+/********** Operations ************ */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 3);
+// console.log( 2 % 2);
+
+let str1 = "hello"
+let str2 = " chakru"
+let str3 =str1 +str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 +"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(2 + 1 +"1");
+
+
+console.log(+true);
+//console.log(true+); gives error
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+console.log(num1);
+
+
+let gameScore = 100
+gameScore++;
+++gameScore
+
+console.log(gameScore);
+
